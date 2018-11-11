71f464816defd761cd7c33f5c68e38b7406b2e6a
Dalton He <daltonhe@gmail.com>
2018-11-10 17:54:52 -0800

    Changed castling rights storage from string to int

--- src/Zobrist.java
+++ src/Zobrist.java
@@ -1,71 +1,57 @@
 import java.util.Random;
 
 /**
- * 
  * @author Dalton He
  * created 10-30-2018
  */
 public class Zobrist implements Definitions {
-	public static long[][] PIECES = new long[13][120];
-	public static long SIDE;
-	public static long W_SHORT_CASTLE;
-	public static long W_LONG_CASTLE;
-	public static long B_SHORT_CASTLE;
-	public static long B_LONG_CASTLE;
-	public static long[] ENPASSANT = new long[8];
+	public static long[][] pieces;
+	public static long side;
+	public static long[] castling;
+	public static long[] enpassant;
 	
 	/**
-	 * 
+	 * Initializes the random number tables used for zobrist key generation.
 	 */
 	public static void initialize() {
-		Random r = new Random();
+		Random r  = new Random();
+		pieces    = new long[13][120]; // pieces[pieceType][index]
+		castling  = new long[16];      // castling[0bKQkq]
+		enpassant = new long[8];       // enpassant[file]
+		
 		for (int i = 0; i < 13; i++)
 			for (int j = 0; j < 120; j++)
-				PIECES[i][j] = Math.abs(r.nextLong());
+				pieces[i][j] = Math.abs(r.nextLong());
 
-		SIDE = Math.abs(r.nextLong());
-		W_SHORT_CASTLE = Math.abs(r.nextLong());
-		W_LONG_CASTLE = Math.abs(r.nextLong());
-		B_SHORT_CASTLE = Math.abs(r.nextLong());
-		B_LONG_CASTLE = Math.abs(r.nextLong());
+		side = Math.abs(r.nextLong());
+		
+		for (int i = 0; i < 16; i++)
+			castling[i] = Math.abs(r.nextLong());
 		
 		for (int i = 0; i < 8; i++)
-			ENPASSANT[i] = Math.abs(r.nextLong());
-
+			enpassant[i] = Math.abs(r.nextLong());
 	}
 	
 	/**
-	 * 
-	 * @param board
-	 * @return
+	 * Returns the zobrist key of the position.
 	 */
-	public static long getKey(Board board) {
+	public static long getKey(Position pos) {
 		long key = 0;
-		for(int index = a8; index <= h1; index++) {
+		for(int index = SQ_a8; index <= SQ_h1; index++) {
 			if ((index & 0x88) != 0)
 				continue;
 
-			if (board.board[index] != 0)
-				key ^= PIECES[board.board[index] + 6][index];
-
+			if (pos.board[index] != 0)
+				key ^= pieces[pos.board[index] + 6][index];
 		}
-		if (board.sideToMove == BLACK)
-			key ^= SIDE;
+		
+		if (pos.sideToMove == BLACK)
+			key ^= side;
 
-		if (board.castling.contains("K"))
-			key ^= W_SHORT_CASTLE;
+		key ^= castling[pos.castling];
 
-		if (board.castling.contains("k"))
-			key ^= B_SHORT_CASTLE;
-
-		if (board.castling.contains("Q"))
-			key ^= W_LONG_CASTLE;
-
-		if (board.castling.contains("q"))
-			key ^= B_LONG_CASTLE;
-
-		if (board.enpassant != -2)
-			key ^= ENPASSANT[board.enpassant % 16];
+		if (pos.enpassant != SQ_NONE)
+			key ^= enpassant[pos.enpassant % 16];
 
 		return key;
 	}
