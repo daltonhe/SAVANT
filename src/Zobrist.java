9314ee8fc3072a36880500669b23838de19b9440
Dalton He <daltonhe@gmail.com>
2018-11-15 16:19:59 -0800

    Cleanup code

--- src/Zobrist.java
+++ src/Zobrist.java
@@ -4,7 +4,7 @@ import java.util.Random;
  * @author Dalton He
  * created 10-30-2018
  */
-public class Zobrist implements Definitions {
+public class Zobrist implements Types {
 	public static long[][] pieces;
 	public static long side;
 	public static long[] castling;
@@ -38,20 +38,13 @@ public class Zobrist implements Definitions {
 	public static long getKey(Position pos) {
 		long key = 0;
 		for(int index = SQ_a8; index <= SQ_h1; index++) {
-			if ((index & 0x88) != 0)
-				continue;
-
+			if ((index & 0x88) != 0) continue;
 			if (pos.board[index] != 0)
 				key ^= pieces[pos.board[index] + 6][index];
 		}
-		
-		if (pos.sideToMove == BLACK)
-			key ^= side;
-
+		if (pos.sideToMove == BLACK) key ^= side;
 		key ^= castling[pos.castling];
-
-		if (pos.enpassant != SQ_NONE)
-			key ^= enpassant[pos.enpassant % 16];
+		if (pos.enpassant != SQ_NONE) key ^= enpassant[pos.enpassant % 16];
 
 		return key;
 	}
